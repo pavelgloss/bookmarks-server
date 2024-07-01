5b05821c94cba8f16723d2d3b0e68e5f1d59273a
pavelg <pavelg@truckersreport.com>
2024-07-01 22:04:00 +0200

    fix port

--- src/main.ts
+++ src/main.ts
@@ -32,7 +32,7 @@ async function bootstrap() {
 
   const port = process.env.PORT || 3000;
   await app.listen(port);
-  console.log(`Bookmarks Server is running on http://localhost:${serverPort}`);
+  console.log(`Bookmarks Server is running on http://localhost:${port}`);
 }
 
 bootstrap();
